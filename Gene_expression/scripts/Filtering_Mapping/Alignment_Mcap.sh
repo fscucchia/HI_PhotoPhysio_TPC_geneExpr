fd9dcc7dfbc0d5b10c4f4b9292e3d7f4791a616e
Federica Scucchia <federica.scucchia@studio.unibo.it>
2025-09-10 16:52:45 -0400

    Update Alignment_Mcap.sh

--- Gene_expression/scripts/Filtering_Mapping/Alignment_Mcap.sh
+++ Gene_expression/scripts/Filtering_Mapping/Alignment_Mcap.sh
@@ -18,7 +18,7 @@ module load STAR/2.7.11b-GCC-12.3.0
 
 #genome='/work/pi_hputnam_uri_edu/HI_Genomes/MCapV3/Montipora_capitata_HIv3.assembly.fasta'
 #star_genome_dir='/scratch/workspace/federica_scucchia_uri_edu-hawaii/20250424_ENCORE_HawaiiTPC_Federica/output/GenomeIndex_Mcap'
-#gtf='/work/pi_hputnam_uri_edu/HI_Genomes/MCapV3/Montipora_capitata_HIv3.genes_fixed.gff3'
+#gtf='/work/pi_hputnam_uri_edu/snRNA_analysis/references/Montipora_capitata_HIv3.gtf'
 #genomeSAindexNbases=13
 #limitSjdbInsertNsj=1200000 # default 1000000
 
